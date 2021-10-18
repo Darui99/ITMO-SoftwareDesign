81451b90e6281d7585a10eda1ab07b27478d4e24
Egor Kurbatov <avangard4700@mail.ru>
2021-10-18 18:56:35 +0300

    Add html abstraction

--- HW3/src/main/java/database/Attribute.java
+++ HW3/src/main/java/database/Attribute.java
@@ -4,6 +4,7 @@ public class Attribute {
     private final String name;
     private final String type;
     private final boolean nullable;
+    private String value;
 
     public Attribute(final String name, final String type, final boolean nullable) {
         this.name = name;
@@ -19,5 +20,21 @@ public class Attribute {
             res += "NOT NULL";
         }
         return res;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
     }
 }
--- HW3/src/main/java/database/QueriesBuilder.java
+++ HW3/src/main/java/database/QueriesBuilder.java
@@ -11,4 +11,30 @@ public class QueriesBuilder {
         res.append(")");
         return res.toString();
     }
+
+    public static String generateInsertQuery(final String name, Attribute... attributes) {
+        StringBuilder res = new StringBuilder();
+        res.append("INSERT INTO ").append(name);
+        res.append("(");
+        for (int i = 0; i < attributes.length; i++) {
+            res.append(attributes[i].getName());
+            if (i != attributes.length - 1) {
+                res.append(", ");
+            }
+        }
+        res.append(")").append(" VALUES ");
+        res.append("(");
+        for (int i = 0; i < attributes.length; i++) {
+            if (attributes[i].getType().equals("TEXT")) {
+                res.append("\"").append(attributes[i].getValue()).append("\"");
+            } else {
+                res.append(attributes[i].getValue());
+            }
+            if (i != attributes.length - 1) {
+                res.append(", ");
+            }
+        }
+        res.append(")");
+        return res.toString();
+    }
 }
